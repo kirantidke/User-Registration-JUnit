5394cce5ea0161959b110131e7a5427e4f25da9e
kirantidke <kirantidke2013@gmail.com>
2021-07-02 23:30:51 +0530

    "[kiran] added user registration file"

---
+++ src/main/java/com/bridgelabz/junit/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.junit;
+
+/**
+ * Welcome!
+ *
+ */
+public class UserRegistration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to user Registration system!" );
+    }
+}
