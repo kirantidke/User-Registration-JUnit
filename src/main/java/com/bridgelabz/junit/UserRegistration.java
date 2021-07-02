19db3e93f71a438d5c312ba1526db05868917b35
kirantidke <kirantidke2013@gmail.com>
2021-07-03 00:28:04 +0530

    "[kiran] added code for valid Email id"

--- src/main/java/com/bridgelabz/junit/UserRegistration.java
+++ src/main/java/com/bridgelabz/junit/UserRegistration.java
@@ -6,17 +6,27 @@ package com.bridgelabz.junit;
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class UserRegistration 
-{
-	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
-    public boolean validateFirstName(String fname) {
-        return patternChecker(fname, NAME_PATTERN);
-    }
 
-    private boolean patternChecker(String input,String fieldPattern) {
-        Pattern pattern = Pattern.compile(fieldPattern);
-        Matcher matcher = pattern.matcher(input);
-        boolean result = matcher.matches();
-        return result;
-    }
+public class UserRegistration {
+	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
+
+	public boolean validateFirstName(String fname) {
+		return patternChecker(fname, NAME_PATTERN);
+	}
+
+	public boolean validateLastName(String lName) {
+		return patternChecker(lName, NAME_PATTERN);
+	}
+
+	public boolean valudateEmailId(String emailId) {
+		return patternChecker(emailId, EMAIL_PATTERN);
+	}
+
+	private boolean patternChecker(String input, String fieldPattern) {
+		Pattern pattern = Pattern.compile(fieldPattern);
+		Matcher matcher = pattern.matcher(input);
+		boolean result = matcher.matches();
+		return result;
+	}
 }
--- src/test/java/com/bridgelabz/junit/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/junit/UserRegistrationTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for User Registration.
  */
 public class UserRegistrationTest {
 	UserRegistration userRegistration = new UserRegistration();
@@ -34,5 +34,34 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.validateFirstName("Kiran@");
 		Assert.assertFalse(result);
 	}
+	@Test
+	public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue(){
+	    boolean result = userRegistration.validateLastName("Tidke");
+	    Assert.assertTrue(result);
+	}
+	  @Test
+	  public void givenLastName_WhenLessThanThreeChars_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateFirstName("Ti");
+	      Assert.assertFalse(result);
+	  }
+
+	  @Test
+	  public void givenLastName_WhenHasNumber_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateFirstName("Tidke1");
+	      Assert.assertFalse(result);
+	  }
+
+	  @Test
+	  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateFirstName("Tidke@");
+	      Assert.assertFalse(result);
+	  }
+	
+	 @Test
+	    public void givenEmailId_WhenProper_ShouldReturnTrue(){
+	        boolean result = userRegistration.valudateEmailId("abc.syz@bl.co.in");
+	        Assert.assertTrue(result);
+	    }
+
 
 }
