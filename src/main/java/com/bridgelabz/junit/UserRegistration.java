1871ced557c184f01fee1ea83c65212fff595770
kirantidke <kirantidke2013@gmail.com>
2021-07-03 00:05:04 +0530

    "[kiran] added code for valid LastName"

--- src/main/java/com/bridgelabz/junit/UserRegistration.java
+++ src/main/java/com/bridgelabz/junit/UserRegistration.java
@@ -1,13 +1,26 @@
 package com.bridgelabz.junit;
 
 /**
- * Welcome!
+ * Ability to check valid last name
  *
  */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to user Registration system!" );
-    }
+	 private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+	    public boolean validateFirstName(String fName) {
+	        return patternChecker(fName, NAME_PATTERN);
+	    }
+	    public boolean validateLastName(String lName){
+	        return patternChecker(lName, NAME_PATTERN);
+	    }
+
+
+	    private boolean patternChecker(String input,String fieldPattern) {
+	        Pattern pattern = Pattern.compile(fieldPattern);
+	        Matcher matcher = pattern.matcher(input);
+	        boolean result = matcher.matches();
+	        return result;
+	    }
 }
---
+++ src/test/java/com/bridgelabz/junit/UserRegistrationTest.java
@@ -0,0 +1,59 @@
+package com.bridgelabz.junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserRegistrationTest 
+{ UserRegistration userRegistration = new UserRegistration();
+@Test
+public void givenFirstName_WhenFirstLatterUpperCase_ShouldReturnTrue() {
+ boolean result = userRegistration.validateFirstName("Kiran");
+ Assert.assertTrue(result);
+}
+
+@Test
+public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse() {
+ boolean result = userRegistration.validateFirstName("Ka");
+ Assert.assertFalse(result);
+}
+
+@Test
+public void givenFirstName_WhenHasNumber_ShouldReturnFalse() {
+ boolean result = userRegistration.validateFirstName("Kira1");
+ Assert.assertFalse(result);
+}
+
+@Test
+public void givenFirstName_WhenHasSpecialChar_ShouldReturnFalse() {
+ boolean result = userRegistration.validateFirstName("Kiran@");
+ Assert.assertFalse(result);
+}
+@Test
+public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue(){
+    boolean result = userRegistration.validateLastName("Tidke");
+    Assert.assertTrue(result);
+}
+  @Test
+  public void givenLastName_WhenLessThanThreeChars_ShouldReturnFalse() {
+      boolean result = userRegistration.validateFirstName("Ti");
+      Assert.assertFalse(result);
+  }
+
+  @Test
+  public void givenLastName_WhenHasNumber_ShouldReturnFalse() {
+      boolean result = userRegistration.validateFirstName("Tidke1");
+      Assert.assertFalse(result);
+  }
+
+  @Test
+  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse() {
+      boolean result = userRegistration.validateFirstName("Tidke@");
+      Assert.assertFalse(result);
+  }
+
+}
